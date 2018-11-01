e63b20227453effc2e93abc3a4359e2734182238
Mustafe Biihi <Mustafebiihi@gmail.com>
2018-11-01 12:35:48 +0200

    Initial commit

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-// Serve static files
-app.use(express.static(__dirname + '/src'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/my-app'));
 
-// Send all requests to index.html
-app.get('/', function(req, res) {
-  res.render('index.html');
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/my-app/index.html'));
 });
 
-// default Heroku port
-app.listen(5000);
-console.log("Server is listen on port");
-console.log(__dirname);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
